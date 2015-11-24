8a6163766db92494d430cd4c40d7411e749e4fd0
Vlad Golubev <golubev.vld@gmail.com>
2015-11-25 00:05:03 +0200

    Delivery class stub implementation

--- src/ua/vladgolubev/department/delivery/Delivery.java
+++ src/ua/vladgolubev/department/delivery/Delivery.java
@@ -1,4 +1,47 @@
 package ua.vladgolubev.department.delivery;
 
+import ua.vladgolubev.agreement.Agreement;
+
+import java.time.LocalDateTime;
+
 public class Delivery {
+    private String destinationLocation;
+    private Agreement agreement;
+    private LocalDateTime estimatedDate;
+    private boolean isExpired;
+
+    public Delivery(String destinationLocation, LocalDateTime estimatedDate) {
+        this.destinationLocation = destinationLocation;
+        this.estimatedDate = estimatedDate;
+    }
+
+    public void setAgreement(Agreement agreement) {
+        this.agreement = agreement;
+    }
+
+    public String getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public Agreement getAgreement() {
+        return agreement;
+    }
+
+    public LocalDateTime getEstimatedDate() {
+        return estimatedDate;
+    }
+
+    public boolean isExpired() {
+        return isExpired;
+    }
+
+    @Override
+    public String toString() {
+        return "Delivery{" +
+                "destinationLocation='" + destinationLocation + '\'' +
+                ", agreement=" + agreement +
+                ", estimatedDate=" + estimatedDate +
+                ", isExpired=" + isExpired +
+                '}';
+    }
 }

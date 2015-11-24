bf8faf85aec68ae7f543287d166c7723f09c5692
Vlad Golubev <golubev.vld@gmail.com>
2015-11-24 18:27:48 +0200

    Stub class implementation for AgreementSpecification

--- src/ua/vladgolubev/agreement/AgreementSpecification.java
+++ src/ua/vladgolubev/agreement/AgreementSpecification.java
@@ -1,4 +1,12 @@
 package ua.vladgolubev.agreement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgreementSpecification {
+    private List<AgreementSpecificationItem> items = new ArrayList<AgreementSpecificationItem>();
+
+    public void addSpecification(AgreementSpecificationItem item) {
+        items.add(item);
+    }
 }

818e35895b2b407c41fb5c12cdbb25a1d8f8fa91
Vlad Golubev <golubev.vld@gmail.com>
2015-11-29 17:05:45 +0200

    Implement DeliveryPlan class

--- src/ua/vladgolubev/department/delivery/DeliveryPlan.java
+++ src/ua/vladgolubev/department/delivery/DeliveryPlan.java
@@ -1,4 +1,33 @@
 package ua.vladgolubev.department.delivery;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeliveryPlan {
+    private List<Delivery> plannedDeliveries = new ArrayList<>();
+    private LocalDate lastPossibleDate = LocalDate.MIN;
+
+    public void planDelivery(Delivery delivery) {
+        plannedDeliveries.add(delivery);
+        if (delivery.getEstimatedDate().compareTo(lastPossibleDate) > 0) {
+            lastPossibleDate = delivery.getEstimatedDate();
+        }
+    }
+
+    public List<Delivery> getPlannedDeliveries() {
+        return plannedDeliveries;
+    }
+
+    public LocalDate getLastPossibleDate() {
+        return lastPossibleDate;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryPlan{" +
+                "plannedDeliveries=" + plannedDeliveries +
+                ", lastPossibleDate=" + lastPossibleDate +
+                '}';
+    }
 }

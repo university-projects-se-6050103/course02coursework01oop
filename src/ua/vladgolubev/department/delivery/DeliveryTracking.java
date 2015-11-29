7a780a9af539a0d7218704af7395abd450a88b6b
Vlad Golubev <golubev.vld@gmail.com>
2015-11-29 17:10:38 +0200

    Implement DeliveryTracking class

--- src/ua/vladgolubev/department/delivery/DeliveryTracking.java
+++ src/ua/vladgolubev/department/delivery/DeliveryTracking.java
@@ -1,4 +1,14 @@
 package ua.vladgolubev.department.delivery;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DeliveryTracking {
+    public List<Delivery> getOverdueDeliveries(DeliveryPlan deliveryPlan) {
+        return deliveryPlan.getPlannedDeliveries()
+                .stream()
+                .filter(delivery -> delivery.getEstimatedDate().compareTo(LocalDate.now()) > 0)
+                .collect(Collectors.toList());
+    }
 }
